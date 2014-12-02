59bee309b7f0f1c561b936e6555d9a3f7756b398
deadprogram <ron@hybridgroup.com>
2014-12-02 14:47:08 -0800

    Better idea of what I mean here

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
-module.exports = {
-  createClient: function(client) {
-    client['drop'] = function() {
-    	console.log('drop');
-    };
+var request = require('request');
 
-    client['load'] = function() {
-    	console.log('load');
-    };
-
-    client['close'] = function() {
-    	console.log('close');
-    };
-  }
+var DroneDrop = function() {
+  this.host = '192.168.1.1';
 };
+
+DroneDrop.prototype.createClient = function(client) {
+	var that = this;
+	// TODO: get host from client
+
+	client['drop'] = that.drop;
+	client['load'] = that.load;
+	client['close'] = that.close;
+};
+
+DroneDrop.prototype.connect = function(host) {
+	this.host = host;
+};
+
+DroneDrop.prototype.drop = function() {
+	console.log('drop');
+	request(this.host + '/api/commands/drop');
+};
+
+DroneDrop.prototype.load = function() {
+	console.log('load');
+	request(this.host + '/api/commands/load');
+};
+
+DroneDrop.prototype.close = function() {
+	console.log('close');
+	request(this.host + '/api/commands/close');
+};
+
+module.exports = new DroneDrop();
