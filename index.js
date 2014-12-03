bdb9b8751d315ff3c3af6eb971085f0bfdfd841a
deadprogram <ron@hybridgroup.com>
2014-12-03 12:13:30 -0800

    Grab IP address from the nodecopter client, and some refactors

--- index.js
+++ index.js
@@ -2,34 +2,38 @@ var request = require('request');
 
 var DroneDrop = function() {
   this.host = '192.168.1.1';
+  this.port = '8080'
 };
 
 DroneDrop.prototype.createClient = function(client) {
 	var that = this;
-	// TODO: get host from client
+	this.host = client._options.ip || '192.168.1.1';
 
 	client['drop'] = that.drop;
 	client['load'] = that.load;
 	client['close'] = that.close;
 };
 
-DroneDrop.prototype.connect = function(host) {
-	this.host = host;
+DroneDrop.prototype.connect = function(host, port) {
+	this.host = host || '192.168.1.1';
+	this.port = port || '8080';
 };
 
 DroneDrop.prototype.drop = function() {
-	console.log('drop');
-	request.get(this.host + '/api/commands/drop');
+	this._sendCommand('drop');
 };
 
 DroneDrop.prototype.load = function() {
-	console.log('load');
-	request.get(this.host + '/api/commands/load');
+	this._sendCommand('load');
 };
 
 DroneDrop.prototype.close = function() {
-	console.log('close');
-	request.get(this.host + '/api/commands/close');
+	this._sendCommand('close');
+};
+
+DroneDrop.prototype._sendCommand = function(command) {
+	console.log(command);
+	request.get(this.host + ':' + this.port + '/api/commands/' + command);
 };
 
 module.exports = new DroneDrop();
